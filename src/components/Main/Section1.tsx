c7d81e1670c4e5bd265b638ebc8ba3d7ebac99de
Lucca Rauédys <luccarauedys@outlook.com>
2023-04-30 13:22:34 -0300

    feat: add primeira secao do conteudo principal do site

---
+++ src/components/Main/Section1.tsx
@@ -0,0 +1,14 @@
+export default function Section1() {
+  return (
+    <section className="container mx-auto bg-[url('/bg-icosahedron.png')] bg-contain bg-center bg-no-repeat">
+      <div className="container flex flex-col gap-6 lg:gap-8 justify-center items-center text-center py-4 md:py-12">
+        <h3>Welcome to Cronos</h3>
+        <h1>The Internet of Blockchains.</h1>
+        <p>
+          Cronos is an ever expanding ecosystem of connected apps and services,
+          built for a decentralized future.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import Section1 from "./Section1";
+
+export default function Main() {
+  return (
+    <main className="px-4 md:px-8">
+      <Section1 />
+    </main>
+  );
+}
