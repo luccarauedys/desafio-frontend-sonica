8cefd4dbd27c85dd59ea7aa49163f82f4378b0e0
Lucca Rauédys <luccarauedys@outlook.com>
2023-04-30 19:35:22 -0300

    fix: corrigindo problema de responsividade

--- src/components/Main/Section2.tsx
+++ src/components/Main/Section2.tsx
@@ -3,16 +3,17 @@ import { CaretRight } from "@phosphor-icons/react";
 
 export default function Section2() {
   return (
-    <section className="container grid gap-12 grid-cols-1 md:grid-cols-3 relative md:py-12">
+    <section className="container grid gap-12 md:grid-cols-3 md:py-12 relative">
       <Image
         src={"/helix.png"}
         alt="helix icon"
         width={400}
         height={400}
-        className="hidden md:block w-[280px] lg:w-[400px] absolute -z-10 top-0 -left-20 lg:-left-10"
-        data-aos="fade-right"
+        className="absolute -z-10 top-0 -left-20 lg:-left-10 filter blur-sm w-[250px] lg:w-[400px]"
+        data-aos="fade-up"
       />
-      <div className="container md:col-span-2 flex flex-col gap-4 lg:gap-8" data-aos="fade-right">
+
+      <div className="container col-span-2 flex flex-col gap-4 lg:gap-8" data-aos="fade-up">
         <h4>Community-owned and operated</h4>
         <h2>Enter a Universe of Connected Services.</h2>
         <p className="max-w-none">
@@ -28,15 +29,12 @@ export default function Section2() {
         </div>
       </div>
 
-      <div
-        className="container flex flex-col sm:flex-row md:flex-col gap-8 sm:gap-12 justify-center text-center"
-        data-aos="fade-left"
-      >
-        <div>
+      <div className="justify-self-center col-span-2 md:col-span-1 grid gap-4" data-aos="fade-up">
+        <div className="flex flex-col items-end">
           <p className="text-5xl lg:text-7xl font-extrabold default-text-gradient">265+</p>
           <p>Apps & services</p>
         </div>
-        <div>
+        <div className="flex flex-col items-end">
           <p className="text-5xl lg:text-7xl font-extrabold default-text-gradient">$63B+</p>
           <p>Digital assets</p>
         </div>
